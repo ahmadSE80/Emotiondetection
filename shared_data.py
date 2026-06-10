aab527f280db971e2e82cf878ef8de97534d65cc
ahmadSE80 <ahmadbhutta878@gmail.com>
2026-06-10 15:16:54 +0500

    Add share data file

---
+++ shared_data.py
@@ -0,0 +1,17 @@
+# shared_data.py
+
+students = []
+
+engagement_score = 0
+
+current_frame = None
+paused = False
+emotion_counts = {
+    "Happy": 0,
+    "Neutral": 0,
+    "Sad": 0,
+    "Surprise": 0,
+    "Angry": 0,
+    "Fear": 0,
+    "Disgust": 0
+}
